88799c02deb24d5ec257da8ea3010254fb6b445e
Frère Jacques <zyb@zjex.com>
2016-11-23 12:45:04 +0800

    Lesson 3 Home Work  option 2 done

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+  isValidEmail:Ember.computed.match('email',/^.+@.+\..+$/),
+  isValidMEssage:Ember.computed.gte('message.length',5),
+  isValid:Ember.computed.and('isValidEmail','isValidMEssage')
 });
