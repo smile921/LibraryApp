b8f05893552b68a418dca609b9ab89e6b92a3bcf
Frère Jacques <zyb@zjex.com>
2016-11-23 11:22:39 +0800

    Lesson 3 Home Work  option 1 done
    option 1: Further improve your Contact Page.
    
    Create a contact model with email and message fields.
    Save the model on the server when someone clicks on the “Send” button on the Contact form. Update your contact.js controller to contain validations and actions.
    Create an Admin page under http://localhost:4200/admin/contacts
    List all saved messages in a table.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -17,17 +17,10 @@ export default Ember.Controller.extend({
 			//alert(`We got your message and we’ll get in touch soon ${this.get('message')}`);
 			//let model =  new Contact();
 			debugger;
+			let model =  this.store.createRecord('contact');
 			model.set('email',this.get('emailAddress'));
-			this.saveSendMessage(model);
-		},
-		saveSendMessage(contactData){
-			contactData.save().then(()=> this.transitionTo('index'));
-		},
-		willTransition(){
-			let model = this.controller.get('model');
-			if(model.get('isNew')){
-				model.destroyRecord();
-			}
-		}
+			model.set('message',this.get('message'));
+		 	model.save().then(()=> this.transitionToRoute('index'));
+		} 
 	}
 });
