6bdf9a6c2e0e38008716efacc6f26b455cc5abc8
Frère Jacques <zyb@zjex.com>
2016-11-23 12:21:07 +0800

    change controller logic to route

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
  
+	model(){		 
+		return this.store.createRecord('contact');
+	},
+	actions:{
+		sendMessage(newContact){		 
+			debugger;			 
+		 	newContact.save().then(()=> this.transitionTo('index'));
+		},
+		willTransition(){
+			let model = this.controller.get('model');
+			if(model.get('isNew')){
+				model.destroyRecord();
+			}
+		}
+	}
 	
 });
